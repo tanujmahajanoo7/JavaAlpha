e8e6f52528312d0d71ed6381bd9766a2c80f434e
Tanuj Mahajan <tanujkmahajan@gmail.com>
2025-04-01 09:19:08 +0530

    "Recursion"

---
+++ Questions/IncreasingOrderNo.java
@@ -0,0 +1,14 @@
+public class IncreasingOrderNo {
+    public static void printIncreasing(int n) {
+        if(n==1) {
+            System.out.print(n + " ");
+            return;
+        }
+        printIncreasing(n-1);
+        System.out.print(n + " ");
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printIncreasing(n);
+    }
+}

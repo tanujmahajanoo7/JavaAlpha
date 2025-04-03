8d08f1972cd85a1e9fe3f6888f1f8cbd3296995d
Tanuj Mahajan <tanujkmahajan@gmail.com>
2025-04-03 19:32:58 +0530

    Fibonacci Index Number Using recursion

---
+++ Questions/FibonacciNumber.java
@@ -0,0 +1,21 @@
+public class FibonacciNumber {
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fibnm1 = fib(n - 1);
+        int fibnm2 = fib(n - 2);
+        return fibnm1 + fibnm2;
+    }
+
+    public static void main(String[] args) {
+        int n = 25;
+        System.out.println(fib(n));
+    }
+}
+
+// Time Complexity: O(2^n)
+// The function makes two recursive calls for each non-base case, leading to an
+// exponential growth in calls.
+// Space Complexity: O(n)
+// The maximum depth of the recursion stack is proportional to the input `n`.

854f93730748e0a28383594ade58241ed2225f65
Tanuj Mahajan <tanujkmahajan@gmail.com>
2025-05-12 22:58:00 +0530

    friendsParing

---
+++ Questions/Recursion/FriendParingProblem.java
@@ -0,0 +1,23 @@
+// Given n friends, each one can remain single or can be paired up with some other friend. 
+// Each friend can be paired only once. 
+// Find out the total number of ways in which friends can remain single or can be paired up.
+// ANS :- (fn-1) + (fn-1) * (fn-2)
+
+public class FriendParingProblem {
+
+    public static int friendsParing(int n) {
+        if (n == 1 || n == 2)
+            return n;
+
+        int fnm1 = friendsParing(n - 1);
+        int fnm2 = friendsParing(n - 2);
+        int pairWays = (n - 1) * fnm2;
+        int totalways = fnm1 + pairWays;
+        return totalways;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(friendsParing(3));
+    }
+
+}
--- test.java
+++ test.java
@@ -1,8 +1,23 @@
-import java.util.Scanner;
 class test {
-    public static String FirstLetterUpperCase(String str) {
-        
+
+    public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            removeDuplicates(str, idx + 1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx + 1, newStr.append(currChar), map);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbbccd";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
 
-folder 19
+// folder 1974

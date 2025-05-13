6abae4a556118df54dc159304ef767148679e2d5
Tanuj Mahajan <tanujkmahajan@gmail.com>
2025-05-13 22:54:49 +0530

    arrangement

---
+++ Recursion/Questions/KeyIndexNumber.java
@@ -0,0 +1,20 @@
+// package Recursion.Questions;
+// You are given an array of integers. Your task is to find all the positions (indices) within this array where a specific integer (let's call it the 'key') appears. You need to accomplish this using a recursive function. Finally, you need to print out all these indices.
+// Input Array: [3, 2, 4, 5, 6, 2, 7, 2, 2]
+// Key Element: 2
+// Output: 1 5 7 8.
+
+public class KeyIndexNumber {
+
+    public static void keyIndexNumber(int i, int key, int[] arr) {
+        if (i == arr.length) {
+            System.out.println(arr.toString());
+            return;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

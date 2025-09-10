cb2b66766125c6f473e1e224671e9a1698fd20dc
Tanuj Mahajan <tanujkmahajan@gmail.com>
2025-09-10 22:44:57 +0530

    test

--- test.java
+++ test.java
@@ -1,34 +1,4 @@
-// folder 23 start before rivision all the things
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
+// folder 23.12 start before rivision all the things
 class test {
 
-    public static int maxSubArray(int[] nums) {
-        int ms = nums[0];
-        int cs = nums[0];
-
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        while(sc.hasNext()) {
-            int num = sc.nextInt();
-            arr.add(num);
-        }
-        int a[] = new int[arr.size()];
-
-        for(int i=0; i<arr.size();i++) {
-            a[i] = arr.get(i);
-        }
-
-        System.out.println(maxSubArray(a));
-
-        sc.close(); 
-    }
 }

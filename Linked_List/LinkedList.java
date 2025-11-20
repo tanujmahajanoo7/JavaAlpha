5e755fbccfe75ad397c8095447ad2ad50c8fae48
Tanuj Mahajan <tanujkmahajan@gmail.com>
2025-11-20 08:13:20 +0530

    print linked list

--- Linked_List/LinkedList.java
+++ Linked_List/LinkedList.java
@@ -41,12 +41,31 @@ public class LinkedList {
         tail = newNode;
     }
 
+    // Time Complexity O(n)
+    public void print() {
+        if(head == null) {
+            System.out.print("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();   
+        ll.print();
         ll.addFirst(2);
+        ll.print();
         ll.addFirst(1);
+        ll.print();
         ll.addLast(3);
+        ll.print();
         ll.addLast(4);
+        ll.print();
 
         // System.out.println(ll);
     }
